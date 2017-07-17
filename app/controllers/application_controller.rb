5d335853c8b2087a802f2c8c4f09fba5630ffc4b
Josh Thompson <thompsonjoshd@gmail.com>
2017-07-17 15:57:45 -0600

    clear user's session if not found in DB

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,9 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @user ||= User.find(session[:user_id]) if session[:user_id]
-    # rescue ActiveRecord::RecordNotFound
-    #   session.clear
-    # return
+    rescue ActiveRecord::RecordNotFound
+      session.clear
+    return
   end
 
   def authorize
