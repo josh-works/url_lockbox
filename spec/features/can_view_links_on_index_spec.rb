32214af60bd74a1cd5aeb23b0ad846eadcd1c877
Josh Thompson <thompsonjoshd@gmail.com>
2017-07-17 16:00:41 -0600

    tests pass, can view user's links

--- spec/features/can_view_links_on_index_spec.rb
+++ spec/features/can_view_links_on_index_spec.rb
@@ -3,14 +3,14 @@ require "rails_helper"
 RSpec.describe "viewing user's links" do
   it "should load all user's links" do
     user = create(:user_with_links)
+    links = user.links
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit root_path
 
     within('.links_list') do
       expect(page).to have_css(".link_item", count: 3)
+      expect(page).to have_content(links.first.url)
     end
-
-
   end
 end
