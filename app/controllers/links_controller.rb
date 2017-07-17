300d1bb934d021a4487fbf4976d9dc9e9bb17d31
Josh Thompson <thompsonjoshd@gmail.com>
2017-07-17 15:56:41 -0600

    rendering table of user's links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,7 +3,7 @@ class LinksController < ApplicationController
   before_action :authorize
 
   def index
-
+    @links = current_user.links
   end
 
 end
--- spec/features/can_view_links_on_index_spec.rb
+++ spec/features/can_view_links_on_index_spec.rb
@@ -1,10 +1,16 @@
 require "rails_helper"
 
 RSpec.describe "viewing user's links" do
-  xit "should load all user's links" do
+  it "should load all user's links" do
     user = create(:user_with_links)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    expect(user.links.count).to eq(3)
+    visit root_path
+
+    within('.links_list') do
+      expect(page).to have_css(".link_item", count: 3)
+    end
+
+
   end
 end
