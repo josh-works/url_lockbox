628dcd7ce4aa4df04d4af65908096a31535adfa7
Josh Thompson <thompsonjoshd@gmail.com>
2017-07-27 10:47:32 -0600

    red feature test to toggle link read/unread

---
+++ spec/features/user_can_mark_links_read_unread_spec.rb
@@ -0,0 +1,42 @@
+require "rails_helper"
+
+RSpec.describe "user can mark links as read" do
+
+  before :each do
+    @user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    link = create(:link)
+    @user.links << link
+  end
+
+  scenario "user can mark unread link as read" do
+
+    visit root_path
+
+    within('.links_list') do
+      expect(page).to_not have_css('.read')
+      click_on("Mark Link as Read")
+
+    end
+
+    within('.link_item') do
+      expect(page).to have_css('.read')
+    end
+  end
+
+  xscenario "user can mark read link as unread" do
+
+    visit root_path
+
+    within('.links_list') do
+      expect(page).to have_css('.read')
+      click_on("Mark Link as Unread")
+
+    end
+
+    within('.link_item') do
+      expect(page).to_not have_css('.read')
+    end
+  end
+
+end
